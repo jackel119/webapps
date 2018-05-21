c56f9e25be47bd553a853ba2488f86f09eb9ef13
Zicong Ma <zm1216@ic.ac.uk>
2018-05-21 15:27:58 +0100

    addition of the initial config file and setup for fb authentication

---
+++ config/config.js
@@ -0,0 +1,12 @@
+const fb = require('fb_auth');
+
+module.exports={
+  "facebook_api_key"      :     "1699398683492193",
+  "facebook_api_secret"   :     fb.secret,
+  "callback_url"          :     "http://localhost:3000/auth/facebook/callback",
+  "use_database"          :     "false",
+  "host"                  :     "localhost",
+  "username"              :     "root",
+  "password"              :     "",
+  "database"              :     "DB NAME"
+}
