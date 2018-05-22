3498559ba5d0d1934e1e9561cf175ad0bb1b7780
Jack Pordi <jackel119@gmail.com>
2018-05-22 15:41:52 +0100

    Started work on database queries

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var express           =     require('express');
-var socket            =     require('socket.io');
-var passport          =     require('passport');
-var util              =     require('util');
-var FacebookStrategy  =     require('passport-facebook').Strategy;
-var session           =     require('express-session');
-var cookieParser      =     require('cookie-parser');
-var bodyParser        =     require('body-parser');
-var config            =     require('./config/config');
-var mysql             =     require('mysql');
-var app               =     express();
+var express           =  require('express');
+var socket            =  require('socket.io');
+var passport          =  require('passport');
+var util              =  require('util');
+var FacebookStrategy  =  require('passport-facebook').Strategy;
+var session           =  require('express-session');
+var cookieParser      =  require('cookie-parser');
+var bodyParser        =  require('body-parser');
+var config            =  require('./config/config');
+var app               =  express();
+var pg                =  require('./database');
+
+const db = new pg.Database();
+console.log(db.name);
 
 var server = app.listen(2605, () => {
   console.log("Listening to requests on port 2605");
@@ -20,4 +23,19 @@ var io = socket(server);
 
 io.on('connection', (socket) => {
   console.log('Made socket connection with socket:', socket.id);
+
+  socket.on('query', (query) => {
+    console.log("Data UID:", query.uid);
+    db.client.query("SELECT * FROM \"USER\" WHERE UID = $1\;", [query.uid], (err, res) => {
+      console.log(res);
+      socket.emit('message', {
+        message: res.rows[0].first_name
+      });
+    });
+  });
+
+  socket.on('test-packet', (data) => {
+    console.log(data.message);
+  });
+
 });
--- public/connect.js
+++ public/connect.js
@@ -1,1 +1,15 @@
 var socket = io.connect("http://localhost:2605");
+
+var gid = document.getElementById('gid');
+    button = document.getElementById('request');
+
+
+button.addEventListener('click', function() {
+  socket.emit("query", {
+    uid: gid.value,
+  });
+});
+
+socket.on('message', (data) => {
+  gid.value = data.message;
+});
