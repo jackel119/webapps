cddcf1bc63b5a53fcd66d51b7ffc1537d8e3c567
Jack Pordi <jackel119@gmail.com>
2018-05-29 18:25:06 +0100

    Same as last commit

--- public/connect.js
+++ public/connect.js
@@ -1,4 +1,4 @@
-var socket = io.connect("http://localhost:2605");
+var socket = io.connect("http://www.jackpordi.com:2605");
 
 var gid = document.getElementById('gid');
     button = document.getElementById('request');
