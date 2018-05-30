caa998906dba829de6bf89f5ed964c6612f08b85
Jack Pordi <jackel119@gmail.com>
2018-05-30 02:19:13 +0100

    Trying https with lets encrypt

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var fs                =  require('fs');
 var express           =  require('express');
 var socket            =  require('socket.io');
 var passport          =  require('passport');
@@ -9,14 +10,57 @@ var bodyParser        =  require('body-parser');
 var config            =  require('./config/config');
 var pg                =  require('./database');
 var app               =  express();
+var GEX               =  require('greenlock-express');
+var https             =  require('https');
 
+// Database Setup
 const db = new pg.Database('webapp-testing');
 
-var server = app.listen(2605, () => {
-  console.log("Listening to requests on port 2605");
+
+//-------------------------------------
+//--------EXPRESS SERVER---------------
+//-------------------------------------
+var server = https.createServer({
+  key : fs.readFileSync('/etc/letsencrypt/live/jackpordi.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/jackpordi.com/fullchain.pem')
+}, app).listen(443, () => {
+  console.log("Listening to requests on port 443");
 });
 
 app.use(express.static('public'));
+
+//-------------------------------------
+//--------WEBSOCKET STUFF--------------
+//-------------------------------------
+
+var io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('Made socket connection with socket:', socket.id);
+
+  socket.on('query', (query) => {
+    console.log("Data UID:", query.uid);
+    db.client.query("SELECT * FROM \"USER\" WHERE UID = $1\;", [query.uid], (err, res) => {
+      console.log(res);
+      socket.emit('message', {
+        message: res.rows[0].first_name
+      });
+    });
+  });
+
+  socket.on('test-packet', (data) => {
+    console.log(data.message);
+  });
+
+  socket.on('hello-message', (data) => {
+    console.log(data.message);
+  });
+
+});
+
+//-------------------------------------
+//--------AUTHENTICATION STUFF---------
+//-------------------------------------
 
 // Set passport to use FacebookStrategy
 passport.use(
@@ -48,30 +92,3 @@ app.get(
     res.send('AUTH WAS GOOD!');
   }
 );
-
-var io = socket(server);
-
-io.on('connection', (socket) => {
-  console.log('Made socket connection with socket:', socket.id);
-
-  socket.on('query', (query) => {
-    console.log("Data UID:", query.uid);
-    db.client.query("SELECT * FROM \"USER\" WHERE UID = $1\;", [query.uid], (err, res) => {
-      console.log(res);
-      socket.emit('message', {
-        message: res.rows[0].first_name
-      });
-    });
-  });
-
-  socket.on('test-packet', (data) => {
-    console.log(data.message);
-  });
-
-  socket.on('hello-message', (data) => {
-    console.log(data.message);
-  });
-
-
-});
-
